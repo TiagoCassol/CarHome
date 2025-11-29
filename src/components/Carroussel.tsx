da7fdf5ed1d74cd91f4f075a8fbb3d35462ccd90
Simon <simontsui85@gmail.com>
2025-11-29 19:04:50 -0300

    Changed Carroussel, easier to see

--- src/components/Carroussel.tsx
+++ src/components/Carroussel.tsx
@@ -14,15 +14,15 @@ export default function Carroussel() {
     const showDiv = (index: SetStateAction<number>) => {
         setSlideIndex(index);
     }
-    
+
     return (
         <div className="w3-content w3-display-container" style={{ maxWidth: "1200" }}>
             {images.map((image, index) =>
                 <img key={index} className="mySlides" src={image} style={{ width: "100%", display: slideIndex == index ? 'block' : 'none' }} />
             )}
             <div className="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style={{ width: "100%" }}>
-                <div className="w3-left w3-hover-text-khaki" onClick={() => plusDiv(-1)}>&#10094;</div>
-                <div className="w3-right w3-hover-text-khaki" onClick={() => plusDiv(1)}>&#10095;</div>
+                <div className="w3-left w3-text-black" onClick={() => plusDiv(-1)}>&#10094;</div>
+                <div className="w3-right w3-text-black" onClick={() => plusDiv(1)}>&#10095;</div>
                 {images.map((_image, index) =>
                     <span key={index} className="w3-badge demo w3-border w3-transparent w3-hover-white" onClick={() => showDiv(index)}></span>
                 )}
